83f5fab0aa002b79ea647a18fb9f0649e082aa33
qajybai <qajybai@gmail.com>
2025-09-20 17:49:36 +0500

    leetcode easy 1

---
+++ src/main/java/berrx/algo/TwoSum.java
@@ -0,0 +1,61 @@
+package berrx.algo;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1 TWO SUM
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ *
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ *
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ */
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> idx = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (idx.containsKey(need)) {
+                return new int[] { idx.get(need), i };
+            }
+            idx.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+
+        var result = twoSum.twoSum(new int[]{0,2,5,3,6}, 6);
+
+        Arrays.stream(result).forEach(System.out::println);
+    }
+
+}
